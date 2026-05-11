c58f7f63c72b887fcb6c564dc379c169b15a59f0
Bardon <bardvioel@gmail.com>
2026-05-11 20:27:05 +0500

    Исправлен баг с длиной пароля

--- frontend/src/Utils/validation/validation.ts
+++ frontend/src/Utils/validation/validation.ts
@@ -8,7 +8,7 @@ export const validateEmail = (email: string): string => {
 
 export const validatePassword = (password: string): string => {
   if (!password) return 'Введите пароль';
-  if (password.length < 6) return 'Не менее 6 символов';
+  if (password.length < 8) return 'Минимум 8 символов'; // было 6
   return '';
 };
 
