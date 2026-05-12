13ebb80f1f491db0cae11f95dc134af894723c37
Ivanuel <ladyginivan0@gmail.com>
2026-05-12 19:15:31 +0500

    feat: Клик на аватрку

--- frontend/src/Components/Pages/HomePage/components/Header/header.tsx
+++ frontend/src/Components/Pages/HomePage/components/Header/header.tsx
@@ -1,24 +1,31 @@
 import { memo } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Styles.module.scss";
 import { Button } from "@/Components/UI/Button/Button";
+import { useAuth } from "@/Auth";
 import activBell from "/Bell_Active.svg";
 import basicBell from "/Bell.svg";
+import defaultAvatar from "/masscot-main.png";
 import cn from "classnames";
 
 interface HeaderProps {
-  userName: string;
-  userAvatar?: string;
   isActivBell?: boolean;
   className?: string;
 }
 
-const HeaderComponent = ({ userName, userAvatar, isActivBell, className }: HeaderProps) => {
+const HeaderComponent = ({ isActivBell, className }: HeaderProps) => {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
   const greeting = () => {
     const hour = new Date().getHours();
     if (hour < 12) return "Доброе утро";
     if (hour < 18) return "Добрый день";
     return "Добрый вечер";
   };
+
+  const avatarSrc = /*user?.avatar || */ defaultAvatar;
+  const userName = user?.username || "Пользователь";
 
   return (
     <div className={cn(styles.header, className)}>
@@ -28,10 +35,10 @@ const HeaderComponent = ({ userName, userAvatar, isActivBell, className }: Heade
       </div>
       <div className={styles.buttons}>
         <Button color="accent-2" className={styles.avatar_button}>
-          <img className={styles.userAvatar} src={userAvatar} alt="avatar" loading="eager" decoding="async" />
+          <img className={styles.userAvatar} src={userAvatar} alt="avatar" loading="eager" decoding="async" onClick={() => navigate('/settings')}/>
         </Button>
         <Button color="accent-2" className={styles.bell_button}>
-          <img className={styles.bell} src={isActivBell ? activBell : basicBell}/>
+          <img className={styles.bell} src={isActivBell ? activBell : basicBell} alt="уведомления"/>
         </Button>
       </div>
     </div>
