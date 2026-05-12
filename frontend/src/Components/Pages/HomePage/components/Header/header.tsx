f86c524600d39265c2e49347012e6b70cae55181
Ivanuel <ladyginivan0@gmail.com>
2026-05-12 19:22:39 +0500

    feat: Клик на аватрку

--- frontend/src/Components/Pages/HomePage/components/Header/header.tsx
+++ frontend/src/Components/Pages/HomePage/components/Header/header.tsx
@@ -1,32 +1,27 @@
 import { memo } from "react";
-import { useNavigate } from "react-router-dom";
 import styles from "./Styles.module.scss";
 import { Button } from "@/Components/UI/Button/Button";
-import { useAuth } from "@/Auth";
 import activBell from "/Bell_Active.svg";
 import basicBell from "/Bell.svg";
-import defaultAvatar from "/masscot-main.png";
 import cn from "classnames";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
+  userName: string;
+  userAvatar?: string;
   isActivBell?: boolean;
   className?: string;
 }
 
-const HeaderComponent = ({ isActivBell, className }: HeaderProps) => {
-  const navigate = useNavigate();
-  const { user } = useAuth();
-
+const HeaderComponent = ({ userName, userAvatar, isActivBell, className }: HeaderProps) => {
   const greeting = () => {
     const hour = new Date().getHours();
     if (hour < 12) return "Доброе утро";
     if (hour < 18) return "Добрый день";
     return "Добрый вечер";
   };
-
-  const avatarSrc = /*user?.avatar || */ defaultAvatar;
-  const userName = user?.username || "Пользователь";
-
+  const navigate = useNavigate();
+  
   return (
     <div className={cn(styles.header, className)}>
       <div>
@@ -38,7 +33,7 @@ const HeaderComponent = ({ isActivBell, className }: HeaderProps) => {
           <img className={styles.userAvatar} src={userAvatar} alt="avatar" loading="eager" decoding="async" onClick={() => navigate('/settings')}/>
         </Button>
         <Button color="accent-2" className={styles.bell_button}>
-          <img className={styles.bell} src={isActivBell ? activBell : basicBell} alt="уведомления"/>
+          <img className={styles.bell} src={isActivBell ? activBell : basicBell}/>
         </Button>
       </div>
     </div>
